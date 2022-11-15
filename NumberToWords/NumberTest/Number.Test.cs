b2cebd82d43efda046adc1386cfcdb127d620a98
RemyXVX <remyfranciscoflores@gmail.com>
2022-11-15 09:09:38 -0800

    Test pass, was able to create a dictionary

---
+++ NumberToWords/NumberTest/Number.Test.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Numbers.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Numbers.Tests
+{
+  [TestClass]
+  public class IntegerTest
+  {
+    [TestMethod]
+    public void CreatingDictionary()
+    {
+    
+    }
+
+  }
+}
---
+++ NumberToWords/Numbers/Models/Number.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Numbers.Model
+{
+  public class Integer
+  {
+    static Dictionary<int, string> singles = new Dictionary<int, string>() {
+      {1, "one"}
+    };
+  }
+}
